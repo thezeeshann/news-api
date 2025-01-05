64384f0c910c1d8f8da6140ddc27f7ac9a51bba8
thezeeshann <info.zeeshann@gmail.com>
2025-01-06 01:50:11 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from "express";
+import authRoute from "./routes/auth";
+import cloudinaryConnect from "./lib/cloudinary";
+import fileUpload from "express-fileupload";
 import dotnev from "dotenv";
 dotnev.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+cloudinaryConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "temp",
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
@@ -14,6 +24,8 @@ app.get("/", (req: Request, res: Response) => {
     message: "Your server is up and running....",
   });
 });
+
+app.use("/api/v1/auth", authRoute);
 
 app.listen(port, () => {
   console.log(
