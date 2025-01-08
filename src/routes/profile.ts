76a03a46ced745e5d72ea3e8be2acf276b94a086
thezeeshann <info.zeeshann@gmail.com>
2025-01-08 22:56:11 +0530

    refactor: update profile routes

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { getUserProfile } from "../controllers/profile-controllers";
+import {
+  getUserProfile,
+  updateProfile,
+} from "../controllers/profile-controllers";
+import { verifyToken } from "../lib/verify-token";
 
 const router = express.Router();
 router.get("/profile/:userId", getUserProfile);
+router.put("/update/:userId", verifyToken, updateProfile);
 
 export default router;
